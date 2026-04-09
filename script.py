01a5ec10913cbeacd0c77f828c5d09a3e8abfe2d
mukundchoudhary28 <mukund.ch28@gmail.com>
2026-04-09 18:26:20 +0530

    Fixed Notebook rendering

--- script.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("slm_colab.ipynb", "r", encoding="utf-8") as f:
-    nb = json.load(f)
-
-nb.get("metadata", {}).pop("widgets", None)
-
-with open("slm_colab_clean.ipynb", "w", encoding="utf-8") as f:
-    json.dump(nb, f, indent=2)
